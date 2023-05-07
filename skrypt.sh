1742149d0551b9989d7624ece838c4f167ab1bef
LeszekBo <132820778+LeszekBo@users.noreply.github.com>
2023-05-07 15:04:37 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [[ "$1" == "--date" ]]; then
-	date +"Dzisiaj jest %A, %d, %B, %Y"
+if [[ "$1" == "--logs" && "$2"" =~ ^[0-9]+$ ]]; then
+	for (( i=1; i<=$2; i++)): do
+		echo "log$i.txt" > log$1.txt
+		echo "$0" >> log$i.txt
+		echo "$(date)" >> log$i.txt
+	done
 else
 	exit 1
 fi
